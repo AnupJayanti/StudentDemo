2d4f497a7cd59af519183c68fa4c58e51f9a825c
Jayanti Kulkarni <Jayanti.Kulkarni@ig.com>
2020-08-26 07:31:21 +0530

    test

---
+++ src/test/java/com/jayanti/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayanti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
